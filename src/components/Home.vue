5b649536ab979042f00731c26283bf08e7ba091a
chajinjoo <chajanee@gmail.com>
2020-04-15 18:06:40 +0900

    feat: 초기세팅 & 기본 레이아웃 구성
    - assets 폴더에 폰트,이미지,공통 css 정리
    - 컴포넌트 분리 & 기본적인 화면 레이아웃 구성
    
    https://trello.com/c/dLE2kJaP

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <span class="contact">contact@formakers.net</span>
+        <div class="cardContainer">
+            <div class="card">
+                <span class="firstSub">사전예약 없는!<br>신작 게임 플레이</span>
+                <span class="secondSub">바로 플레이 하세요.</span>
+                <img class="cardImg" src="../assets/images/intro-img-1.png" alt="">
+            </div>
+            <div class="card">
+                <span class="firstSub">탈락 없는!<br>베타 테스트 참여</span>
+                <span class="secondSub">자유롭게 참여 가능해요.</span>
+                <img class="cardImg" src="../assets/images/intro-img-2.png" alt="">
+            </div>
+            <div class="card">
+                <span class="firstSub">짭짤해!<br>포메스 리워드</span>
+                <span class="secondSub">게임 피드백주고 보상을 받으세요.</span>
+                <img class="cardImg" src="../assets/images/intro-img-3.png" alt="">
+            </div>
+        </div>
+        <div class="logoContainer">
+            <div>
+                <img class="colorBar" src="../assets/images/color-bar.png" alt="">
+            </div>
+            <div>
+                <img class="fomesLogo" src="../assets/images/fomes.png" alt="">
+            </div>
+            <span class="fomesSub">신작게임 놀이터, 포메스</span>
+            <div>
+                <img class="download" src="../assets/images/download.png" alt="">
+            </div>
+        </div>
+        <span class="copyright">Copyright © 2020 ForMakers</span>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss" src="./Home.scss">
+
+</style>
